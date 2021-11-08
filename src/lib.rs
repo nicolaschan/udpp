8b1333a1bf1497f24cce678080210cb69c90c21e
Nicolas Chan <nicolas@nicolaschan.com>
2021-11-07 20:13:52 -0800

    omg it passes

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,17 @@ mod tests {
             .unwrap()
     }
 
+    async fn setup_channel_test() -> (SocketAddr, UdppServer, UdppSession) {
+        let addr = socket_addr("127.0.0.1:8080");
+        let (sender1, receiver1) = unbounded();
+        let (sender2, receiver2) = unbounded();
+
+        let server = UdppServer::new(Box::new(sender1), Box::new(receiver2));
+        let conn = UdppSession::new(Box::new(sender2), Box::new(receiver1), addr).await;
+
+        (addr, server, conn)
+    }
+
     // #[test]
     // fn handles_empty_data() {
     //     let (sender1, receiver1) = unbounded();
@@ -50,11 +61,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_client_sends_to_server() {
-        let (sender1, receiver1) = unbounded();
-        let (sender2, receiver2) = unbounded();
-
-        let server = UdppServer::new(Box::new(sender1), Box::new(receiver2));
-        let mut conn = UdppSession::new(Box::new(sender2), Box::new(receiver1), socket_addr("127.0.0.1:8080")).await;
+        let (_addr, server, mut conn) = setup_channel_test().await;
 
         let data: Vec<u8> = vec![1,2,3,4];
         let expected = data.clone();
@@ -64,5 +71,41 @@ mod tests {
         conn.send(data).await;
         let actual = server_conn.recv().await;
         assert_eq!(expected, actual);
+    }
+
+    #[tokio::test]
+    async fn test_server_sends_to_client() {
+        let (_addr, server, mut conn) = setup_channel_test().await;
+
+        let data: Vec<u8> = vec![1,2,3,4];
+        let expected = data.clone();
+        let mut incoming = server.incoming();
+        let mut server_conn = incoming.accept().await;
+
+        server_conn.send(data).await;
+        let actual = conn.recv().await;
+        assert_eq!(expected, actual);
+    }
+
+    #[tokio::test]
+    async fn test_bidirectional() {
+        let (_addr, server, mut conn) = setup_channel_test().await;
+
+        let data: Vec<u8> = vec![1,2,3,4];
+        let expected = data.clone();
+        let data2: Vec<u8> = vec![1,2,3,4];
+        let expected2 = data2.clone();
+        let mut incoming = server.incoming();
+        let mut server_conn = incoming.accept().await;
+
+        server_conn.send(data).await;
+        conn.send(data2).await;
+
+        let actual = conn.recv().await;
+        assert_eq!(expected, actual);
+
+        let actual2 = server_conn.recv().await;
+        assert_eq!(expected2, actual2);
+
     }
 }
